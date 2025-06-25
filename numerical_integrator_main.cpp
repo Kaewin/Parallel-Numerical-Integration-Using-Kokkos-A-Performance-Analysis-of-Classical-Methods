ca29f8b40785dcfccd83059748fccedebc4cd887
kaewin <kaelyn_parris@protonmail.com>
2025-06-25 17:45:16 -0500

    Added in benchmarking.

--- numerical_integrator_main.cpp
+++ numerical_integrator_main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include <functional>
+#include <chrono>
 using namespace std;
+
+enum Method {
+    RECTANGLE,
+    TRAPEZOIDAL
+};
+
+struct BenchmarkResult {
+    double result;
+    double timeMs;
+};
+
+
 
 class NumericalIntegrator {
     public:
@@ -27,6 +40,31 @@ class NumericalIntegrator {
 
             return sum;
         }
+
+        double integrate(const std::function<double(double)>& f, double a, double b, int n, Method method) {
+            switch (method) {
+                case Method::RECTANGLE:
+                    return rectangleRule(f, a, b, n);
+                case Method::TRAPEZOIDAL:
+                    return trapezoidalRule(f, a, b, n);
+                default:
+                    return trapezoidalRule(f, a, b, n);
+            }    
+        }   
+
+        BenchmarkResult benchmark(const std::function<double(double)>& f, double a, double b, int n, Method method) {
+
+            auto start = chrono::high_resolution_clock::now();
+
+            double result = integrate(f, a, b, n, method);
+
+            auto end = chrono::high_resolution_clock::now();
+
+            auto duration = chrono::duration_cast<chrono::microseconds>(end - start);
+            double timeMs = duration.count() / 1000.0;
+
+            return {result, timeMs};
+        }
 };
 
 double testFunction(double x) { 
@@ -39,10 +77,13 @@ int main() {
     // Test: integrate x^2 from 0 to 1
     // Answer should be 0.333333
 
-    double result = integrator.rectangleRule(testFunction, 0.0, 1.0, 1000000000);
-    cout << "Rectangle rule result: " << result << endl;
-    result = integrator.trapezoidalRule(testFunction, 0.0, 1.0, 1000000000);
-    cout << "Trapezoidal rule result: " << result;
+    auto benchmark_result1 = integrator.benchmark(testFunction, 0.0, 1.0, 100000000, RECTANGLE);
+    cout << "Result: " << benchmark_result1.result << endl;
+    cout << "Time: " << benchmark_result1.timeMs << " ms" << endl;
+
+    auto benchmark_result2 = integrator.benchmark(testFunction, 0.0, 1.0, 100000000, TRAPEZOIDAL);
+    cout << "Result: " << benchmark_result2.result << endl;
+    cout << "Time: " << benchmark_result2.timeMs << " ms" << endl;
 
     return 0;
 }
